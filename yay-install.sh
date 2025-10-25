a5eb785f57984488145e5234afcc346458291102
sever0815 <sergeilemonow@protonmail.com>
2025-10-26 02:15:54 +0500

    edit

--- yay-install.sh
+++ yay-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
